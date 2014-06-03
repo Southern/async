14e6a55990f90cfed22f8aea156ea50fa768aab4
Colton Baker <sly@roguebandit.co>
2014-06-03 19:04:01 -0400

    Add mapper

---
+++ map/map.go
@@ -0,0 +1,37 @@
+package mapper
+
+import (
+  "github.com/Southern/async"
+  "reflect"
+)
+
+func Map(data interface{}, routine async.Routine, callbacks ...async.Done) error {
+  var (
+    routines []async.Routine
+    results  []interface{}
+  )
+
+  d := reflect.ValueOf(data)
+
+  for i := 0; i < d.Len(); i++ {
+    v := d.Index(i).Interface()
+    routines = append(routines, func(done async.Done, args ...interface{}) {
+      done = func(original async.Done) async.Done {
+        return func(err error, args ...interface{}) {
+          results = append(results, args...)
+          if i == d.Len() {
+            original(err, results...)
+            return
+          }
+          original(err, args...)
+        }
+      }(done)
+
+      routine(done, v)
+    })
+  }
+
+  async.Waterfall(routines, callbacks...)
+
+  return nil
+}
---
+++ map/map_test.go
@@ -0,0 +1,68 @@
+package mapper
+
+import (
+  "fmt"
+  "github.com/Southern/async"
+  "testing"
+)
+
+func TestMapString(t *testing.T) {
+  str := []string{
+    "test",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+  }
+
+  mapper := func(done async.Done, args ...interface{}) {
+    println("Hit string")
+    fmt.Printf("Args: %+v\n", args)
+    done(nil, args...)
+  }
+
+  final := func(err error, results ...interface{}) {
+    println("Hit string end")
+    fmt.Printf("Results: %+v\n", results)
+  }
+
+  Map(str, mapper, final)
+}
+
+func TestMapInt(t *testing.T) {
+  ints := []int{
+    1, 2, 3, 4, 5,
+  }
+
+  mapper := func(done async.Done, args ...interface{}) {
+    println("Hit bool")
+    fmt.Printf("Args: %+v\n", args)
+    done(nil, args...)
+  }
+
+  final := func(err error, results ...interface{}) {
+    println("Hit string end")
+    fmt.Printf("Results: %+v\n", results)
+  }
+
+  Map(ints, mapper, final)
+}
+
+func TestMapBool(t *testing.T) {
+  bools := []bool{
+    true, false, false, true, false,
+  }
+
+  mapper := func(done async.Done, args ...interface{}) {
+    println("Hit bool")
+    fmt.Printf("Args: %+v\n", args)
+    done(nil, args...)
+  }
+
+  final := func(err error, results ...interface{}) {
+    println("Hit string end")
+    fmt.Printf("Results: %+v\n", results)
+  }
+
+  Map(bools, mapper, final)
+}
