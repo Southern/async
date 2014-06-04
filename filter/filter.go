33c175a14efd88880d9ebc48747a72ba53ed4e64
Colton Baker <sly@roguebandit.co>
2014-06-04 19:20:36 -0400

    Add Filter function; works like Map with some tweaks

---
+++ filter/filter.go
@@ -0,0 +1,39 @@
+package filter
+
+import (
+  "fmt"
+  "git.aviuslabs.net/golang/async"
+  "reflect"
+)
+
+func Filter(data interface{}, routine async.Routine, callbacks ...async.Done) {
+  var (
+    routines []async.Routine
+    results  []interface{}
+  )
+
+  d := reflect.ValueOf(data)
+
+  for i := 0; i < d.Len(); i++ {
+    v := d.Index(i).Interface()
+    routines = append(routines, func(done async.Done, args ...interface{}) {
+      done = func(original async.Done) async.Done {
+        return func(err error, args ...interface{}) {
+          fmt.Printf("%+v\n", args[0])
+          if args[0] != false {
+            results = append(results, v)
+          }
+          if i == d.Len() {
+            original(err, results...)
+            return
+          }
+          original(err, args...)
+        }
+      }(done)
+
+      routine(done, v)
+    })
+  }
+
+  async.Waterfall(routines, callbacks...)
+}
---
+++ filter/filter_test.go
@@ -0,0 +1,37 @@
+package filter
+
+import (
+  "fmt"
+  "git.aviuslabs.net/golang/async"
+  "testing"
+)
+
+func TestFilter(t *testing.T) {
+  str := []string{
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+  }
+
+  mapper := func(done async.Done, args ...interface{}) {
+    println("Hit string")
+    fmt.Printf("Args: %+v\n", args)
+    if args[0] == "test3" {
+      done(nil, false)
+      return
+    }
+    done(nil, true)
+  }
+
+  final := func(err error, results ...interface{}) {
+    println("Hit string end")
+    fmt.Printf("Results: %+v\n", results)
+    if results[2] != "test4" {
+      t.Errorf("Did not filter correctly.")
+    }
+  }
+
+  Filter(str, mapper, final)
+}
