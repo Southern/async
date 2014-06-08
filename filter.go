04b7638baa9c7c6b26be7b34301f5fbd4980e25f
Colton Baker <sly@roguebandit.co>
2014-06-07 22:30:40 -0400

    Add FilterParallel

--- filter.go
+++ filter.go
@@ -6,7 +6,7 @@ import (
 
 /*
 
-  Filter out information from a slice.
+  Filter out information from a slice in Waterfall mode.
 
   You must call the Done function with false as its first argument if you do
   not want the data to be present in the results. No other arguments will
@@ -75,6 +75,7 @@ func Filter(data interface{}, routine Routine, callbacks ...Done) {
       return func(done Done, args ...interface{}) {
         done = func(original Done) Done {
           return func(err error, args ...interface{}) {
+
             if args[0] != false {
               results = append(results, v)
             }
@@ -92,4 +93,88 @@ func Filter(data interface{}, routine Routine, callbacks ...Done) {
   }
 
   Waterfall(routines, callbacks...)
+}
+
+/*
+
+  Filter out information from a slice in Parallel mode.
+
+  You must call the Done function with false as its first argument if you do
+  not want the data to be present in the results. No other arguments will
+  affect the performance of this function. When calling the Done function,
+  an error will cause the filtering to immediately exit.
+
+  For example, take a look at one of the tests for this function:
+    func TestFilterStringParallel(t *testing.T) {
+      str := []string{
+        "test1",
+        "test2",
+        "test3",
+        "test4",
+        "test5",
+      }
+
+      expects := []string{
+        "test1",
+        "test2",
+        "test4",
+        "test5",
+      }
+
+      mapper := func(done Done, args ...interface{}) {
+        Status("Hit string")
+        Status("Args: %+v\n", args)
+        if args[0] == "test3" {
+          done(nil, false)
+          return
+        }
+        done(nil, true)
+      }
+
+      final := func(err error, results ...interface{}) {
+        Status("Hit string end")
+        Status("Results: %+v\n", results)
+        for i := 0; i < len(results); i++ {
+          if results[i] != expects[i] {
+            t.Errorf("Did not filter correctly.")
+            break
+          }
+        }
+      }
+
+      FilterParallel(str, mapper, final)
+    }
+
+  Each Routine function will be passed the current value and its index the
+  slice for its arguments.
+
+  The output of filtering in Parallel mode cannot be guaranteed to stay in the
+  same order, due to the fact that it may take longer to process some things
+  in your filter routine. If you need the data to stay in the order it is in,
+  use Filter instead to ensure it stays in order.
+
+*/
+func FilterParallel(data interface{}, routine Routine, callbacks ...Done) {
+  var routines []Routine
+
+  d := reflect.ValueOf(data)
+
+  for i := 0; i < d.Len(); i++ {
+    v := d.Index(i).Interface()
+    routines = append(routines, func(id int) Routine {
+      return func(done Done, args ...interface{}) {
+        done = func(original Done) Done {
+          return func(err error, args ...interface{}) {
+            if args[0] != false {
+              original(err, v)
+            }
+          }
+        }(done)
+
+        routine(done, v, id)
+      }
+    }(i))
+  }
+
+  Parallel(routines, callbacks...)
 }
--- filter_test.go
+++ filter_test.go
@@ -121,3 +121,123 @@ func TestFilterInt(t *testing.T) {
 
   Filter(bools, mapper, final)
 }
+
+func TestFilterStringParallel(t *testing.T) {
+  str := []string{
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+  }
+
+  expects := []string{
+    "test1",
+    "test2",
+    "test4",
+    "test5",
+  }
+
+  mapper := func(done Done, args ...interface{}) {
+    Status("Hit string")
+    Status("Args: %+v\n", args)
+    if args[0] == "test3" {
+      done(nil, false)
+      return
+    }
+    done(nil, true)
+  }
+
+  final := func(err error, results ...interface{}) {
+    Status("Hit string end")
+    Status("Results: %+v\n", results)
+    for i := 0; i < len(results); i++ {
+      if results[i] != expects[i] {
+        t.Errorf("Did not filter correctly.")
+        break
+      }
+    }
+  }
+
+  FilterParallel(str, mapper, final)
+}
+
+func TestFilterBoolParallel(t *testing.T) {
+  bools := []bool{
+    true,
+    false,
+    false,
+    true,
+    false,
+  }
+
+  expects := []bool{
+    true,
+    false,
+    true,
+    false,
+  }
+
+  mapper := func(done Done, args ...interface{}) {
+    Status("Hit bool")
+    Status("Args: %+v\n", args)
+    if args[1] == 2 {
+      done(nil, false)
+      return
+    }
+    done(nil, true)
+  }
+
+  final := func(err error, results ...interface{}) {
+    Status("Hit bool end")
+    Status("Results: %+v\n", results)
+    for i := 0; i < len(results); i++ {
+      if results[i] != expects[i] {
+        t.Errorf("Did not filter correctly.")
+        break
+      }
+    }
+  }
+
+  FilterParallel(bools, mapper, final)
+}
+
+func TestFilterIntParallel(t *testing.T) {
+  bools := []int{
+    1,
+    2,
+    3,
+    4,
+    5,
+  }
+
+  expects := []int{
+    1,
+    2,
+    4,
+    5,
+  }
+
+  mapper := func(done Done, args ...interface{}) {
+    Status("Hit bool")
+    Status("Args: %+v\n", args)
+    if args[0] == 3 {
+      done(nil, false)
+      return
+    }
+    done(nil, true)
+  }
+
+  final := func(err error, results ...interface{}) {
+    Status("Hit bool end")
+    Status("Results: %+v\n", results)
+    for i := 0; i < len(results); i++ {
+      if results[i] != expects[i] {
+        t.Errorf("Did not filter correctly.")
+        break
+      }
+    }
+  }
+
+  FilterParallel(bools, mapper, final)
+}
