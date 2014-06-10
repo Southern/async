c7118e20ef8ca068a5dfa97f4e9dc415e6775a0d
Colton Baker <sly@roguebandit.co>
2014-06-10 14:39:40 -0400

    Add Series

---
+++ series.go
@@ -0,0 +1,112 @@
+package async
+
+/*
+  Shorthand to List.RunSeries without having to manually create a new
+  list, add the routines, etc.
+*/
+func Series(routines []Routine, callbacks ...Done) {
+  l := New()
+  l.Multiple(routines...)
+
+  l.RunSeries(callbacks...)
+}
+
+/*
+  Run all of the Routine functions in a series effect.
+
+  If there is an error, series will immediately exit and trigger the
+  callbacks with the error.
+
+  There are no arguments passed between the routines that are used in series.
+  It is just for commands that need to run asynchronously without seeing the
+  results of its previous routine.
+
+  For example, take a look at one of the tests for this function:
+    func TestSeries(t *testing.T) {
+      counter := 0
+
+      Status("Calling Series")
+      async.Series([]async.Routine{
+        func(done async.Done, args ...interface{}) {
+          Status("Increasing counter...")
+          counter++
+          done(nil)
+        },
+        func(done async.Done, args ...interface{}) {
+          Status("Increasing counter...")
+          counter++
+          done(nil)
+        },
+        func(done async.Done, args ...interface{}) {
+          Status("Increasing counter...")
+          counter++
+          done(nil)
+        },
+        func(done async.Done, args ...interface{}) {
+          Status("Increasing counter...")
+          counter++
+          done(nil)
+        },
+      }, func(err error, results ...interface{}) {
+        if err != nil {
+          t.Errorf("Unexpected error: %s", err)
+          return
+        }
+
+        if counter != 4 {
+          t.Errorf("Not all routines were completed.")
+          return
+        }
+
+        Status("Counter: %d", counter)
+      })
+    }
+
+*/
+func (l *List) RunSeries(callbacks ...Done) {
+  fall := fallSeries(l, callbacks...)
+  next := nextSeries(l, callbacks...)
+
+  l.Wait.Add(l.Len())
+
+  fall(next)
+}
+
+func fallSeries(l *List, callbacks ...Done) func(Done, ...interface{}) {
+  return func(next Done, args ...interface{}) {
+    e := l.Front()
+    _, r := l.Remove(e)
+
+    // Run the first waterfall routine and give it the next function, and
+    // any arguments that were provided
+    go r(next)
+    l.Wait.Wait()
+  }
+}
+
+func nextSeries(l *List, callbacks ...Done) Done {
+  fall := fallSeries(l, callbacks...)
+
+  return func(err error, args ...interface{}) {
+    next := nextSeries(l, callbacks...)
+
+    l.Wait.Done()
+    if err != nil || l.Len() == 0 {
+      // Just in case it's an error, let's make sure we've cleared
+      // all of the sync.WaitGroup waits that we initiated.
+      for i := 0; i < l.Len(); i++ {
+        l.Wait.Done()
+      }
+
+      // Send the results to the callbacks
+      for i := 0; i < len(callbacks); i++ {
+        callbacks[i](err)
+      }
+      return
+    }
+
+    // Run the next waterfall routine with any arguments that were provided
+    fall(next)
+    return
+  }
+}
---
+++ series_test.go
@@ -0,0 +1,81 @@
+package async_test
+
+import (
+  "fmt"
+  "github.com/Southern/async"
+  "testing"
+)
+
+func TestSeries(t *testing.T) {
+  counter := 0
+
+  Status("Calling Series")
+  async.Series([]async.Routine{
+    func(done async.Done, args ...interface{}) {
+      Status("Increasing counter...")
+      counter++
+      done(nil)
+    },
+    func(done async.Done, args ...interface{}) {
+      Status("Increasing counter...")
+      counter++
+      done(nil)
+    },
+    func(done async.Done, args ...interface{}) {
+      Status("Increasing counter...")
+      counter++
+      done(nil)
+    },
+    func(done async.Done, args ...interface{}) {
+      Status("Increasing counter...")
+      counter++
+      done(nil)
+    },
+  }, func(err error, results ...interface{}) {
+    if err != nil {
+      t.Errorf("Unexpected error: %s", err)
+      return
+    }
+
+    if counter != 4 {
+      t.Errorf("Not all routines were completed.")
+      return
+    }
+
+    Status("Counter: %d", counter)
+  })
+}
+
+func TestSeriesError(t *testing.T) {
+  counter := 0
+
+  Status("Calling Series")
+  async.Series([]async.Routine{
+    func(done async.Done, args ...interface{}) {
+      Status("Increasing counter...")
+      counter++
+      done(nil)
+    },
+    func(done async.Done, args ...interface{}) {
+      Status("Sending error...")
+      done(fmt.Errorf("Test error"))
+    },
+    func(done async.Done, args ...interface{}) {
+      Status("Increasing counter...")
+      counter++
+      done(nil)
+    },
+    func(done async.Done, args ...interface{}) {
+      Status("Increasing counter...")
+      counter++
+      done(nil)
+    },
+  }, func(err error, results ...interface{}) {
+    if err == nil {
+      t.Errorf("Did not throw an error as expected")
+      return
+    }
+
+    Status("Got error: %s", err)
+  })
+}
